2c0ae7cac84b5414abead7a5a38c85f2941f85ca
SudiptaSaha <SudiptaSaha@DESKTOP-VF6E5HV>
2017-09-14 14:56:49 +0530

    HelloWorld GIT

---
+++ src/com/sid/main/HelloWorldGIT.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sid.main;
+
+/**
+ * @author SudiptaSaha
+ *
+ */
+public class HelloWorldGIT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World GIT");
+	}
+
+}
